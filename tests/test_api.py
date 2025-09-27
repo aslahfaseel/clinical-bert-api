818a51fbd318d4e6ce457166e5efc569f4ca955d
aslahfaseel <aslahfaseel@gmail.com>
2025-09-27 20:15:02 +0530

    ner

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,25 +1,24 @@
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from fastapi.testclient import TestClient
 from app.main import app
 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 client = TestClient(app)
-
 test_cases = [
     ("The patient denies chest pain.", "ABSENT"),
     ("He has a history of hypertension.", "PRESENT"),
     (
         "If the patient experiences dizziness, reduce the dosage.",
         "PRESENT",
-    ),  # match model output
+    ),
     ("No signs of pneumonia were observed.", "ABSENT"),
 ]
-
 
 def test_predictions():
     for sentence, expected_label in test_cases:
         response = client.post("/predict", json={"sentence": sentence})
         assert response.status_code == 200
         assert response.json()["label"] == expected_label
+        
