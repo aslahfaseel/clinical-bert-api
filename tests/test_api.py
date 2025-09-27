44571d2967196bbf9dce10433929a5aab69a8864
aslahfaseel <aslahfaseel@gmail.com>
2025-09-27 20:26:48 +0530

    gt

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,9 +4,14 @@ from fastapi.testclient import TestClient
 from app.main import app
 
 sys.path.insert(
-    0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    0,
+    os.path.abspath(
+        os.path.join(
+            os.path.dirname(__file__),
+            '..'
+        )
+    )
 )
-
 client = TestClient(app)
 test_cases = [
     ("The patient denies chest pain.", "ABSENT"),
